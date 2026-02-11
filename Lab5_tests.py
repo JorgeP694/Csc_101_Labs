3878bdbb7873968273e5c6103eede667de21b1a1
JP <jpreci05@calpoly.edu>
2026-02-11 11:31:34 -0800

    project 1

---
+++ Lab5_tests.py
@@ -0,0 +1,48 @@
+# Name:
+# Section:
+#########################################################
+import data
+import Lab5
+import unittest
+
+
+# Write your test cases for each part below.
+
+class TestCases(unittest.TestCase):
+	# Task 1
+	
+	
+	# Task 2
+	
+	
+   	# Task 3
+	
+	# Task 4
+	
+	
+	# Task 5
+	
+	# Task 6
+    #    Part 1 tests should be in data_tests.py.
+
+    # Task 7
+    #    Part 2 tests should be in data_tests.py.
+
+
+    # Task 8 
+
+
+    # Task 9
+
+
+    # Task 10
+
+
+    # Task 11
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- data_tests.py
+++ data_tests.py
@@ -1,58 +1,44 @@
+# Name:
+# Section:
+#########################################################
 import data
 import unittest
 
 
 class TestCases(unittest.TestCase):
-    def test_Price_1(self):
-        price = data.Price(1, 20)
-        self.assertEqual(price.dollars, 1)
-        self.assertEqual(price.cents, 20)
+    #### Time tests
+    def test_Time_1(self):
+        time = data.Time(7, 20, 1)
+        self.assertEqual(7, time.hour)
+        self.assertEqual(20, time.minute)
+        self.assertEqual(1, time.second)
 
 
-    def test_Price_2(self):
-        price = data.Price(4, 19)
-        self.assertEqual(price.dollars, 4)
-        self.assertEqual(price.cents, 19)
+    def test_Time_2(self):
+        time = data.Time(4, 19, 45)
+        self.assertEqual(4, time.hour)
+        self.assertEqual(19, time.minute)
+        self.assertEqual(45, time.second)
 
 
-    def test_Price_eq_1(self):
-        price1 = data.Price(1, 20)
-        price2 = data.Price(1, 20)
-        self.assertEqual(price1, price2)
+    #### Add tests for Time.__eq__
+
+    #### Add tests for Time.__repr__
 
 
-    def test_Price_eq_2(self):
-        price1 = data.Price(1, 20)
-        self.assertEqual(price1, price1)
 
 
-    def test_Price_eq_3(self):
-        price1 = data.Price(1, 20)
-        price2 = data.Price(2, 20)
-        self.assertNotEqual(price1, price2)
-
-
-    def test_Price_eq_4(self):
-        price = data.Price(1, 20)
-        other = 1.20
-        self.assertNotEqual(price, other)
-
-
-    def test_Price_repr(self):
-        price = data.Price(5, 75)
-        self.assertEqual(repr(price), 'Price(5, 75)')
-
-
+    #### Point tests
     def test_Point_1(self):
         point = data.Point(7, 20)
-        self.assertAlmostEqual(point.x, 7)
-        self.assertAlmostEqual(point.y, 20)
+        self.assertAlmostEqual(7, point.x)
+        self.assertAlmostEqual(20, point.y)
 
 
     def test_Point_2(self):
         point = data.Point(4, 19)
-        self.assertAlmostEqual(point.x, 4)
-        self.assertAlmostEqual(point.y, 19)
+        self.assertAlmostEqual(4, point.x)
+        self.assertAlmostEqual(19, point.y)
 
 
     def test_Point_eq_1(self):
@@ -80,123 +66,7 @@ class TestCases(unittest.TestCase):
 
     def test_Point_repr(self):
         point = data.Point(5, 75)
-        self.assertEqual(repr(point), 'Point(5, 75)')
-
-
-    def test_Rectangle_1(self):
-        rectangle = data.Rectangle(data.Point(7, 20), data.Point(12, 10))
-        self.assertEqual(rectangle.top_left, data.Point(7, 20))
-        self.assertEqual(rectangle.bottom_right, data.Point(12, 10))
-
-
-    def test_Rectangle_eq_1(self):
-        rectangle1 = data.Rectangle(data.Point(7, 20), data.Point(12, 10))
-        rectangle2 = data.Rectangle(data.Point(7, 20), data.Point(12, 10))
-        self.assertEqual(rectangle1, rectangle2)
-
-
-    def test_Rectangle_eq_2(self):
-        rectangle = data.Rectangle(data.Point(7, 20), data.Point(12, 10))
-        self.assertEqual(rectangle, rectangle)
-
-
-    def test_Rectangle_eq_3(self):
-        rectangle1 = data.Rectangle(data.Point(7, 20), data.Point(12, 10))
-        rectangle2 = data.Rectangle(data.Point(4, 17), data.Point(12, 10))
-        self.assertNotEqual(rectangle1, rectangle2)
-
-
-    def test_Rectangle_eq_4(self):
-        rectangle = data.Rectangle(data.Point(7, 20), data.Point(12, 10))
-        other = 1.20
-        self.assertNotEqual(rectangle, other)
-
-
-    def test_Rectangle_repr(self):
-        rectangle = data.Rectangle(data.Point(7, 20), data.Point(12, 10))
-        self.assertEqual(repr(rectangle),
-            'Rectangle(Point(7, 20), Point(12, 10))')
-
-
-    def test_Circle_1(self):
-        circle = data.Circle(data.Point(7, 20), 6.2)
-        self.assertEqual(circle.center, data.Point(7, 20))
-        self.assertEqual(circle.radius, 6.2)
-
-
-    def test_Circle_eq_1(self):
-        circle1 = data.Circle(data.Point(7, 20), 6.2)
-        circle2 = data.Circle(data.Point(7, 20), 6.2)
-        self.assertEqual(circle1, circle2)
-
-
-    def test_Circle_eq_2(self):
-        circle = data.Circle(data.Point(7, 19), 4.7)
-        self.assertEqual(circle, circle)
-
-
-    def test_Circle_eq_3(self):
-        circle1 = data.Circle(data.Point(7, 20), 4.7)
-        circle2 = data.Circle(data.Point(4, 17), 4.7)
-        self.assertNotEqual(circle1, circle2)
-
-
-    def test_Circle_eq_4(self):
-        circle1 = data.Circle(data.Point(7, 20), 4.7)
-        circle2 = data.Circle(data.Point(7, 20), 4.6)
-        self.assertNotEqual(circle1, circle2)
-
-
-    def test_Circle_eq_5(self):
-        circle = data.Circle(data.Point(7, 20), 4.7)
-        other = 1.20
-        self.assertNotEqual(circle, other)
-
-
-    def test_Circle_repr(self):
-        circle = data.Circle(data.Point(7, 20), 2)
-        self.assertEqual(repr(circle), 'Circle(Point(7, 20), 2)')
-
-
- 
-    def test_Employee_1(self):
-        employee = data.Employee('Neil', 5)
-        self.assertEqual(employee.name, 'Neil')
-        self.assertEqual(employee.pay_rate, 5)
-
-
-    def test_Employee_eq_1(self):
-        employee1 = data.Employee('Neil', 5)
-        employee2 = data.Employee('Neil', 5)
-        self.assertEqual(employee1, employee2)
-
-
-    def test_Employee_eq_2(self):
-        employee = data.Employee('Neil', 5)
-        self.assertEqual(employee, employee)
-
-
-    def test_Employee_eq_3(self):
-        employee1 = data.Employee('Neil', 5)
-        employee2 = data.Employee('Terry', 5)
-        self.assertNotEqual(employee1, employee2)
-
-
-    def test_Employee_eq_4(self):
-        employee1 = data.Employee('Neil', 5)
-        employee2 = data.Employee('Neil', 7)
-        self.assertNotEqual(employee1, employee2)
-
-
-    def test_Employee_eq_5(self):
-        employee = data.Employee('Neil', 5)
-        other = 1.20
-        self.assertNotEqual(employee, other)
-
-
-    def test_Employee_repr(self):
-        employee = data.Employee('Neil', 5)
-        self.assertEqual(repr(employee), "Employee('Neil', 5)")
+        self.assertEqual('Point(5, 75)', repr(point))
 
 
 if __name__ == '__main__':
